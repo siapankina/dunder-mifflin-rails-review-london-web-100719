e99240f0398b00a08431b1b8dc182fffc85b107d
Anastasia Pankina <siapankina@gmail.com>
2019-11-25 09:11:02 +0000

    still need some fixing

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,12 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+    @the_dog=Dog.all.find {|d|
+    d.id==@employee.dog_id}
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  resources :dogs, only: [:index, :show]
+  resources :employees, except: :delete
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
